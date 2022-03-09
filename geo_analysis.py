efa443c137159e6c2c8ffbd6dfe3c7298d8abdf1
gyan <1700010674@pku.edu.cn>
2022-03-08 21:20:24 -0800

    geyan

---
+++ geo_analysis.py
@@ -0,0 +1,46 @@
+import geopandas as gpd
+import pandas as pd
+from matplotlib import pyplot as plt
+from shapely.geometry import Point
+
+
+rent_data = pd.read_csv("data/Rent_Price__LA_.csv")
+crime_data = pd.read_csv("data/Crime_Data_from_2010_to_2019.csv")
+LA_Neighborhood_map = gpd.read_file("data/Los Angeles Neighborhood Map.geojson")
+rent_data_valid = rent_data[["Year", "Amount", "Neighborhood", "Location"]]
+crime_data_valid = crime_data[["DATE OCC", "LAT", "LON", ""]]
+
+crime_data_valid["Year"] = crime_data_valid["DATE OCC"].apply(lambda x: int(x[6:10]))
+crime_data_valid = crime_data_valid[crime_data_valid["Year"] <= 2016]
+rent_data_valid["point"] = rent_data_valid.Location.apply(lambda x: Point(map(float, x.strip("()").split(","))))
+crime_data_valid["point"] = crime_data_valid.loc[:, ["LON", "LAT"]].values.tolist()
+crime_data_geo = gpd.GeoSeries(crime_data_valid["point"].apply(Point))
+LA_Neighborhood_map = LA_Neighborhood_map[LA_Neighborhood_map['geometry'].is_valid]
+del crime_data, rent_data
+
+crime_data_neiborhood = []
+for geo in crime_data_geo:
+    name = (LA_Neighborhood_map["name"])[LA_Neighborhood_map["geometry"].contains(geo)].values
+    crime_data_neiborhood.append(name[0] if name.size else None)
+
+crime_data_valid["Neighborhood"] = pd.Series(crime_data_neiborhood)
+crime_data_valid.to_csv("data/processed_crime.csv")
+rent_data_valid.to_csv("data/processed_rent.csv")
+
+# Analysis
+LA_Neighborhood_map = gpd.read_file("data/Los Angeles Neighborhood Map.geojson")
+crime_data = pd.read_csv("data/processed_crime.csv")
+rent_data = pd.read_csv("data/processed_rent.csv")
+
+LA_Neighborhood_map = LA_Neighborhood_map.set_index("name")
+LA_Neighborhood_map["crime"] = crime_data.groupby("Neiborhood").LAT.count()
+rent_ave = rent_data.groupby("Neighborhood").mean().Amount
+LA_Neighborhood_map["rent"] = rent_ave
+LA_Neighborhood_map.crime.fillna(0, inplace=True)
+LA_Neighborhood_map.plot(column="crime", cmap="Reds", legend=True)
+plt.savefig("crime.png", dpi=1200)
+
+LA_Neighborhood_map.plot(column="rent", cmap="Reds", legend=True)
+plt.savefig("rent.png", dpi=1200)
+
+
